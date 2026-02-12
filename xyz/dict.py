d6c3ec9752eb614ed307d3fed6043a8fae10438c
Rojan <thaparojanthapa1@gmail.com>
2026-02-12 21:51:26 +0545

    Delete xyz directory

--- xyz/dict.py
+++
@@ -1,5 +0,0 @@
-city_map={}
-cities=["Calgary", "Vancouver", "Toronto"]
-city_map["Canada"]=[]
-city_map["Canada"]+=cities
-print(city_map)
--- xyz/doubly_linkedlist.py
+++
@@ -1,3 +0,0 @@
-class doublynode:
-    def __init__(self, val, next=None, prev=None):
-        pass
